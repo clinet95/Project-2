4c093a9db357ff4eb70cccdda58ca271a25bed6b
clinet95 <clinet95@gmail.com>
2020-02-24 22:20:19 -0500

    Updated vms.c toproperly load every memory trace into a linked list. Also created a test.trace file

--- vms.c
+++ vms.c
@@ -2,39 +2,43 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 //An access is every line item in the trace file.
 //It is basically the memory address being requested as well as the type of memory access it is. Read/Write
 struct Access{
 	int bitString[32];
 	char type;
+	struct Access* nextAccess;
 	};
 	
 //Prototype functions
 void loadBitString(char input[], struct Access* access);
 char* decimalToBinary(int input);
-void loadAccesses(char input[]);
+void loadAccesses(char input[], struct Access* a);
+int* stringToIntArray(char input[], int size);
 //personal debug functions
 void printAccessBitString(struct Access* access);
+void printAccesses(struct Access* access); //prints provides access bitstring as well as the accesses linked
 
 
 int main(int argc, char* argv[]){
 	
-	
-	loadAccesses(argv[1]);
-	char input[] = "190aff38"; //created a test address
-	struct Access* access = malloc(sizeof(struct Access)); //create and allocate memory for access
-	//loadBitString(input, access); //convert hexidecimal address into binary
-	printAccessBitString(access); //print access' bit string
+	struct Access* accessHead = malloc(sizeof(struct Access)); //create and allocate memory for access
+
+	loadAccesses(argv[1], accessHead); //reads in every trace from trace file and creates structs for them. Then, creates a linked list out of them
+	printAccesses(accessHead); //prints all accesses starting from the head
+
 	return 0;
 }
 
+//converts decimal to binary
 char* decimalToBinary(int input){
    int c, d, count;
    char *output;
    
    count = 0;
-   output = (char*) malloc(32 + 1);
+   output = (char*) malloc(32 + 1);//chars are one byte, plus additional byte for null char @ end
    
    if (output == NULL)
       exit(1);
@@ -168,23 +172,42 @@ void loadBitString(char input[], struct Access* access){
 		}	
 	}
 }
-void loadAccesses(char input[]){
+void loadAccesses(char input[], struct Access* a){
 	FILE* file;
 	file = fopen(input, "r");
 	if (file == NULL){
 		printf("Error opening file.\n");
 		exit(1);
 	}
+	//used to keep track of previous access pointer
+	struct Access* prevAccess = malloc(sizeof(struct Access)); //create and allocate memory for access
+
 	int lines = 0;
-	while(!feof(file)){
+	while(!feof(file)){ //loop through each line of the file until every line has been read
 		int addr;
 		char rw;
 		fscanf(file,"%x %c", &addr,&rw);
-		if (lines ==0)
-		printf("%s %c\n", decimalToBinary(addr), rw);
+		if (lines ==0){ //create head node
+			int i = 0;
+			for(i; i < 32; i++){
+				a->bitString[i] = decimalToBinary(addr)[i] - '0'; //convert each char value into decimal and store in headAccess
+			}
+			a->nextAccess = NULL;
+			a->type = rw;
+			prevAccess = a;
+		} else { //adding additional nodes
+			struct Access* access = malloc(sizeof(struct Access)); //create and allocate memory for access
+			int i = 0;
+			for(i; i < 32; i++){
+				access->bitString[i] = decimalToBinary(addr)[i] - '0'; //convert each char value into decimal and store in headAccess
+			}
+			prevAccess->nextAccess = access; //set  previous node's "nextaccess" value to current
+			access->nextAccess = NULL;
+			access->type = rw;
+			prevAccess  = access; //now that this node is done, set it as previous so that the next iteration can reference it
+		}
 		lines++;
 	}
-	printf("Lines: %d\n", lines);
 }
 
 //personal debug functions
@@ -198,4 +221,12 @@ void printAccessBitString(struct Access* access){
 			access->bitString[20], access->bitString[21], access->bitString[22], access->bitString[23],
 			access->bitString[24], access->bitString[25], access->bitString[26], access->bitString[27],
 			access->bitString[28], access->bitString[29], access->bitString[30], access->bitString[31]);
+}
+
+void printAccesses(struct Access* access){
+	struct Access* currAccess = access;
+	while(currAccess->nextAccess != NULL){
+		printAccessBitString(currAccess);
+		currAccess = currAccess->nextAccess;
+	}
 }

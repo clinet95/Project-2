27ded20304a1b0ea6e38a622b568b353bdd52919
clinet95 <clinet95@gmail.com>
2020-02-29 23:53:19 -0500

    Updated vms.c. I don't fully have vms Page removal finished

--- vms.c
+++ vms.c
@@ -11,14 +11,26 @@ struct Access{
 	int ramIndex;
 	int hexVal;
 	int isDirty;
+	int referenced;
 	char type;
 	struct Access* nextAccess;
+	struct Access* previousAccess;
 	};
+	
 	
 struct fifoQueue{
 	int key;
 	struct Access* front,* end;
 };	
+
+struct lruList{
+	struct Access* top, *bottom;
+};
+
+struct  vmsQueue{
+	int key;
+	struct Access* front, * end;
+};
 //GLOBAL VARIABLES
 int numOfFrames;
 int* RAM;
@@ -26,9 +38,13 @@ char replacementMethod;
 int debugMode;
 int fifoIndex;
 struct fifoQueue* fifoQueue;
+struct vmsQueue* vmsQueue;
+struct lruList* lruList;
 int eventsInTrace;
 int diskReads;
 int diskWrites;
+int RAMHits;
+int RAMMisses;
 	
 //Prototype functions
 void loadBitString(char input[], struct Access* access);
@@ -40,20 +56,24 @@ void initializeSimulator(char frames[], char mode[], char debug[]);
 void simulate(struct Access* accessHead);
 void insertPage(int pageNum, int frameNum, struct Access* trace);
 struct Access* getAccess(int frameNum, struct Access* accessHead);
+void processTrace(struct Access* trace,struct Access* accessHead);
 
 //page replacement policies here
 int randomPageRemoval();//returns an integer which will be the index of the page's location in RAM
 int fifoPageRemoval(); //returns an integer which will be the index of the page's location in RAM
+int lruPageRemoval();
+int vmsPageRemoval();
 
 //personal debug functions
 void printAccessBitString(struct Access* access);
 void printAccesses(struct Access* access); //prints provides access bitstring as well as the accesses linked
-
+void printLRU();
+void printVMSQueue();
 
 int main(int argc, char* argv[]){
 	
 	struct Access* accessHead = malloc(sizeof(struct Access)); //create and allocate memory for access
-	
+
 	initializeSimulator(argv[2], argv[3], argv[4]);
 	loadAccesses(argv[1], accessHead); //reads in every trace from trace file and creates structs for them. Then, creates a linked list out of them
 	simulate(accessHead);
@@ -213,7 +233,6 @@ void loadBitString(char input[], struct Access* access){
 	}
 }
 void loadAccesses(char input[], struct Access* a){
-	
 	FILE* file;
 	file = fopen(input, "r");
 	if (file == NULL){
@@ -237,8 +256,10 @@ void loadAccesses(char input[], struct Access* a){
 			a->isDirty = 0;
 			a->hexVal = addr;
 			a->nextAccess = NULL;
+			a->previousAccess = NULL;
 			a->type = rw;
 			a->ramIndex = -1;
+			a->referenced = 0;
 			prevAccess = a;
 		} else { //adding additional nodes
 			struct Access* access = malloc(sizeof(struct Access)); //create and allocate memory for access
@@ -246,18 +267,20 @@ void loadAccesses(char input[], struct Access* a){
 			for(i; i < 32; i++){
 				access->bitString[i] = decimalToBinary(addr)[i] - '0'; //convert each char value into decimal and store in headAccess
 			}
+			access->previousAccess = prevAccess;
 			prevAccess->nextAccess = access; //set  previous node's "nextaccess" value to current
 			access->nextAccess = NULL;
-			a->isDirty = 0;
+			access->isDirty = 0;
 			access->hexVal = addr;
 			access->type = rw;
 			access->ramIndex = -1;
+			access->referenced = 0;
 			prevAccess  = access; //now that this node is done, set it as previous so that the next iteration can reference it
 		}
+
 		lines++;
 		eventsInTrace++;
 	}
-	
 	if(debugMode){
 		printf("Successfully loaded accesses from trace file.\n");
 	}
@@ -284,6 +307,8 @@ void initializeSimulator(char frames[], char mode[], char debug[]){
 	eventsInTrace = 0;
 	diskReads = 0;
 	diskWrites = 0;
+	RAMHits = 0;
+	RAMMisses = 0;
 	
 	//allocate array used to act as RAM
 	numOfFrames = atoi(frames);
@@ -292,6 +317,14 @@ void initializeSimulator(char frames[], char mode[], char debug[]){
 	//initialze fifo queue
 	fifoQueue = (struct fifoQueue*)malloc(sizeof(struct fifoQueue)); 
     fifoQueue->front = fifoQueue->end = NULL; 
+	
+	//initialize vmsQueue
+	vmsQueue = (struct vmsQueue*) malloc(sizeof(struct vmsQueue));
+	vmsQueue->front = vmsQueue->end = NULL;
+	
+	//initialize lruList
+	lruList = (struct lruList*)malloc(sizeof(struct lruList));
+	lruList->top = lruList->bottom = NULL;
 	
 	//Fill RAM with -1 to know when RAM is full or not
 	int i;
@@ -333,19 +366,21 @@ void simulate(struct Access* accessHead){
 	if (debugMode){
 		printf("Begginning simulation.\n\n");
 	}
-	int RAMHits = 0, RAMMisses = 0; 
 	struct Access* trace = malloc(sizeof(struct Access*));
-	trace = accessHead;
 	
-	/*trace->ramIndex = accessHead->ramIndex;
+	struct Access* a = (struct Access*)malloc(sizeof(struct Access)); //a is used to iterate through lruList
+	trace->ramIndex = accessHead->ramIndex;
 	trace->type = accessHead->type;
 	trace->hexVal = accessHead->hexVal;
 	trace->isDirty = accessHead->isDirty;
-	int i;
-	for(i = 0; i < 32; i++){
-		trace->bitString[i] = accessHead->bitString[i];
+	trace->nextAccess = accessHead->nextAccess;
+	trace->referenced = accessHead->referenced;
+	trace->previousAccess = accessHead->previousAccess;
+	int j;
+	for(j = 0; j < 32; j++){
+		trace->bitString[j] = accessHead->bitString[j];
 	}
-	*/
+	
 	int tick = 0;
 	//loop through every trace loaded
 	while(trace->nextAccess != NULL){
@@ -387,12 +422,106 @@ void simulate(struct Access* accessHead){
 				break;
 			}
 		}
+				
+		if(trace->type == 'W')
+			trace->isDirty = 1;
+		processTrace(trace, accessHead);
+		printVMSQueue();
+		trace = trace->nextAccess;//increment to next trace
+	}
+	processTrace(trace, accessHead);
+	
+	printVMSQueue();
+	
+	diskReads = RAMMisses;
+	a = accessHead;
+	while(a->nextAccess != NULL){
+		int x;
+		for(x = 0; x < numOfFrames; x++){
+			if(a->ramIndex == x && a->isDirty)
+				diskWrites++;
+		}
+		a = a->nextAccess;
+	}
+}
+void processTrace(struct Access* trace,struct Access* accessHead){
+
+	//if(trace->nextAccess != NULL){
 		int i;
 		int pageFound = 0;
+		if(trace->type == 'W'){
+			trace->isDirty = 1;
+		}
 		//iterate through RAM trying to find current trace's page
 		for(i = 0; i < numOfFrames; i++){
 			if(RAM[i] == getPageNum(trace)){
 				pageFound = 1;
+				//update LRUList
+				if(replacementMethod == 'l'){
+					struct Access* a = (struct Access*)malloc(sizeof(struct Access)); //a is used to iterate through lruList
+					a->ramIndex = lruList->top->ramIndex;
+					a->ramIndex = lruList->top->ramIndex;
+					a->type = lruList->top->type;
+					a->referenced = lruList->top->referenced;
+					a->hexVal = lruList->top->hexVal;
+					a->isDirty = lruList->top->isDirty;
+					a->nextAccess = lruList->top->nextAccess;
+					a->previousAccess = lruList->top->previousAccess;
+					int j;
+					for(j = 0; j < 32; j++){
+						a->bitString[j] = lruList->top->bitString[j];
+					}
+					//loop through lruList to find trace
+					while(a->nextAccess != NULL){
+						if(a->hexVal == trace->hexVal){
+							if(a->previousAccess != NULL){
+								a->previousAccess->nextAccess = a->nextAccess;
+								a->nextAccess->previousAccess = a->previousAccess;
+								lruList->top->previousAccess = a;
+								a->previousAccess = NULL;
+								a->nextAccess = lruList->top;
+								lruList->top = a;							
+							}
+						}
+						a = a->nextAccess;
+					}
+					if(a->hexVal == trace->hexVal){
+								a->previousAccess->nextAccess = NULL;
+								lruList->bottom = a->previousAccess;
+								lruList->top->previousAccess = a;
+								a->nextAccess = lruList->top;
+								lruList->top = a;
+								a->previousAccess = NULL;							
+					}
+					//printLRU();					
+				} else if(replacementMethod == 'v'){
+					struct Access* a = (struct Access*)malloc(sizeof(struct Access)); //a is used to iterate through lruList
+					a->ramIndex = vmsQueue->front->ramIndex;
+					a->type = vmsQueue->front->type;
+					a->referenced = vmsQueue->front->referenced;
+					a->hexVal = vmsQueue->front->hexVal;
+					a->isDirty = vmsQueue->front->isDirty;
+					a->nextAccess = vmsQueue->front->nextAccess;
+					a->previousAccess = vmsQueue->front->previousAccess;
+					int j;
+					for(j = 0; j < 32; j++){
+						a->bitString[j] = vmsQueue->front->bitString[j];
+					}
+					//loop through vmsQueue to find trace
+					while(a->nextAccess != NULL){
+						if(a->hexVal == trace->hexVal){
+							printf("Adding reference bit to: %x\n", a->hexVal);
+							a->referenced = 1;
+							break;
+						}
+						a = a->nextAccess;
+					}
+					if(a->hexVal == trace->hexVal){
+						a->referenced = 1;
+					}
+					printf("%x has a reference bit of: %d\n", a->hexVal, a->referenced);
+
+				}
 			}
 		}
 		
@@ -419,53 +548,45 @@ void simulate(struct Access* accessHead){
 				switch(replacementMethod){
 					case 'r':
 						newFrame = randomPageRemoval(); //generate frame to place trace in
-						//printf("Trying to removeframe: %d\n", newFrame);
-						//check if frame about to be removed is dirty
-						if(getAccess(newFrame, accessHead)->isDirty){
-							getAccess(newFrame, accessHead)->isDirty = 0;
-							diskWrites += 1;
-						}
-						insertPage(getPageNum(trace), newFrame, trace); //place page in frame
 						break;
 					case 'f':
 						newFrame = fifoPageRemoval();
-						//check if frame about to be removed is dirty
-						if(getAccess(newFrame, accessHead)->isDirty){
-							getAccess(newFrame, accessHead)->isDirty = 0;
-							diskWrites += 1;
-						}
-						
-						insertPage(getPageNum(trace), newFrame, trace);
 						break;
 					case 'l':
-					
+						newFrame = lruPageRemoval();
+						if(newFrame == -1){
+							printf("Error. Unable to find page in RAM\n");
+						}
 						break;
 					case'v':
-					
+						newFrame = vmsPageRemoval();
 						break;
 				}
+				//printf("Removing Frame isDirty: %d\n", getAccess(newFrame, accessHead)->isDirty);
+
+				//check if frame about to be removed is dirty
+				struct Access* temp = (struct Access*)malloc(sizeof(struct Access)); //a is used to iterate through lruList
+				temp = getAccess(newFrame, accessHead);
+				//printf("Trace address: %x\n", trace->hexVal);
+				//printf("Trace isDirty: %d\n", trace->isDirty);
+				//printf("Trace ramIndex: %x\n", trace->ramIndex);
+				if(temp->isDirty){
+					temp->isDirty = 0;
+					temp->ramIndex = -1;
+					diskWrites += 1;
+				}
+				insertPage(getPageNum(trace), newFrame, trace); //place page in frame
 			}
 			
 		}
 		
-		
-		
-		
-		trace = trace->nextAccess;//increment tonext trace
-	}
-	
-	diskReads = RAMMisses;
-	if(debugMode){
-		printf("RAM Hits:\t%d\n", RAMHits);
-		printf("RAM Misses:\t%d\n", RAMMisses);
-	}
-	 
+	//}
 	
 }
+
 void insertPage(int pageNum, int frameNum, struct Access* trace){
 	RAM[frameNum] = pageNum;
 	trace->ramIndex = frameNum;
-	//printf("Trace %x  being put in frame %d\n", trace->hexVal, trace->ramIndex);
 	if(trace->type == 'W'){
 		trace->isDirty = 1;
 	}
@@ -476,7 +597,8 @@ void insertPage(int pageNum, int frameNum, struct Access* trace){
 		a->ramIndex = trace->ramIndex;
 		a->type = trace->type;
 		a->hexVal = trace->hexVal;
-		a->isDirty = 0;
+		a->isDirty = trace->isDirty;
+		a->referenced = trace->referenced;
 		int i;
 		for(i = 0; i < 32; i++){
 			a->bitString[i] = trace->bitString[i];
@@ -489,12 +611,70 @@ void insertPage(int pageNum, int frameNum, struct Access* trace){
 			fifoQueue->end->nextAccess = a; 
 			fifoQueue->end = a; 
 		}
+	} else if(replacementMethod == 'l'){
+		//add trace to lruList
+		struct Access* a = (struct Access*)malloc(sizeof(struct Access));
+		a->ramIndex = trace->ramIndex;
+		a->type = trace->type;
+		a->referenced = trace->referenced;
+		a->hexVal = trace->hexVal;
+		a->isDirty = trace->isDirty;
+		int i;
+		for(i = 0; i < 32; i++){
+			a->bitString[i] = trace->bitString[i];
+		}
+		
+		//if list is empty
+		if(lruList->top == NULL){
+			lruList->top = lruList->bottom = a;
+			a->previousAccess = NULL;
+			a->nextAccess = NULL;
+		} else{	//add node to top of list
+			a->nextAccess = lruList->top;
+			lruList->top->previousAccess = a;
+			lruList->top = a;
+			a->previousAccess = NULL;
+
+		}
+		//printLRU();
+	} else if(replacementMethod == 'v'){
+		//add trace to vmsqueue
+		struct Access* a = (struct Access*)malloc(sizeof(struct Access));
+		a->ramIndex = trace->ramIndex;
+		a->type = trace->type;
+		a->hexVal = trace->hexVal;
+		a->isDirty = trace->isDirty;
+		a->referenced = trace->referenced;
+		int i;
+		for(i = 0; i < 32; i++){
+			a->bitString[i] = trace->bitString[i];
+		}
+			// If queue is empty, then new node is front and rear both 
+		if (vmsQueue->end == NULL) { 
+			vmsQueue->front = vmsQueue->end = a; 
+		} else {
+			// Add the new node at the end of queue and change rear 
+			vmsQueue->end->nextAccess = a; 
+			vmsQueue->end = a; 
+		}	
 	}
 }
 struct Access* getAccess(int frameNum, struct Access* accessHead){
 	
-	struct Access* currAccess = accessHead;
-	while(currAccess->nextAccess  != NULL){
+	struct Access* currAccess = (struct Access*)malloc(sizeof(struct Access));
+	currAccess->ramIndex = accessHead->ramIndex;
+	currAccess->ramIndex = accessHead->ramIndex;
+	currAccess->type = accessHead->type;
+	currAccess->referenced = accessHead->referenced;
+	currAccess->hexVal = accessHead->hexVal;
+	currAccess->isDirty = accessHead->isDirty;
+	currAccess->nextAccess = accessHead->nextAccess;
+	currAccess->previousAccess = accessHead->previousAccess;
+	int j;
+	for(j = 0; j < 32; j++){
+		currAccess->bitString[j] = accessHead->bitString[j];
+	}
+	while(currAccess->nextAccess != NULL){
 		if(currAccess->ramIndex == frameNum){
 			return currAccess;
 		}
@@ -504,6 +684,7 @@ struct Access* getAccess(int frameNum, struct Access* accessHead){
 	nullAccess->ramIndex = -1;
 	return nullAccess;
 }
+
 
 int randomPageRemoval(){
 	return rand() % (numOfFrames);
@@ -521,14 +702,82 @@ int fifoPageRemoval(){
         fifoQueue->end = NULL; 
 	int returnVal = temp->ramIndex;
 	if(debugMode){
-		//printf("Evicting %X from index %d.\n", temp->hexVal, temp->ramIndex);
 	}
 	
     free(temp); 
 	return returnVal;
 }
+int lruPageRemoval(){
+	//create a copy of the bottom of the lruList(access that is going to be kicked)
+	struct Access* a = (struct Access*)malloc(sizeof(struct Access)); //a is used to iterate through lruList
+	a->ramIndex = lruList->bottom->ramIndex;
+	a->ramIndex = lruList->bottom->ramIndex;
+	a->type = lruList->bottom->type;
+	a->referenced = lruList->bottom->referenced;
+	a->hexVal = lruList->bottom->hexVal;
+	a->isDirty = lruList->bottom->isDirty;
+	a->nextAccess = lruList->bottom->nextAccess;
+	a->previousAccess = lruList->bottom->previousAccess;
+	int j;
+	for(j = 0; j < 32; j++){
+		a->bitString[j] = lruList->bottom->bitString[j];
+	}
+	
+	//removelruList's bottom and update it
+	lruList->bottom = lruList->bottom->previousAccess;
+	lruList->bottom->nextAccess = NULL;
+	
+	//find index of trace in RAM to return
+	int i, returnVal  = -1;
+	for(i = 0; i < numOfFrames; i++){
+		if(RAM[i] == getPageNum(a)){
+			returnVal = i;
+		}
+	}
+	return returnVal;
+}
 
+int vmsPageRemoval(){
+	// Store previous front and move front one node ahead 
+    struct Access* temp = (struct Access*)malloc(sizeof(struct Access)); 
+	//find first item in queue with no reference
+	temp = vmsQueue->front;
+	while(temp->nextAccess != NULL){
+		if(temp->referenced){
+			printf("Removing reference bits for: %x\n", temp->hexVal);
+			temp->referenced = 0;
+		} else {
+			if(temp == vmsQueue->front){
+				printf("hit front\n");
+				vmsQueue->front = temp->nextAccess;
+				vmsQueue->front->previousAccess = NULL;
+			} else if(temp == vmsQueue->end){
+				printf("hit end\n");
+				vmsQueue->end = vmsQueue->end->previousAccess;
+				vmsQueue->end->nextAccess = NULL;
+			} else {
+				printf("hit mid\n");
+				temp->previousAccess->nextAccess = temp->nextAccess;
+				temp->nextAccess->previousAccess = temp->previousAccess;
+			}
+			
+			break;
+		}
+		temp = temp->nextAccess;
+	}
+			printf("Removing: %x\n", temp->hexVal);
 
+    
+    // If front becomes NULL, then change rear also as NULL 
+    if (vmsQueue->front == NULL) 
+        vmsQueue->end = NULL; 
+	int returnVal = temp->ramIndex;
+	if(debugMode){
+	}
+	
+    free(temp); 
+	return returnVal;
+}
 //personal debug functions
 void printAccessBitString(struct Access* access){
 	printf("%d%d%d%d %d%d%d%d %d%d%d%d %d%d%d%d %d%d%d%d %d%d%d%d %d%d%d%d %d%d%d%d \n",
@@ -547,13 +796,41 @@ void printAccesses(struct Access* access){
 	for(i = 0; i < numOfFrames; i++){
 		printf("%x\n", getAccess(i, access)->hexVal);
 	}
-	/*
-	struct Access* currAccess = access;
-	while(currAccess->nextAccess != NULL){
-		printAccessBitString(currAccess);
-		currAccess = currAccess->nextAccess;
-	}
-	 * */
 }
+void printLRU(){
+			struct Access* a = (struct Access*)malloc(sizeof(struct Access)); //a is used to iterate through lruList
+			a->ramIndex = lruList->top->ramIndex;
+			a->ramIndex = lruList->top->ramIndex;
+			a->type = lruList->top->type;
+			a->referenced = lruList->top->referenced;
+			a->hexVal = lruList->top->hexVal;
+			a->isDirty = lruList->top->isDirty;
+			a->nextAccess = lruList->top->nextAccess;
+			a->previousAccess = lruList->top->previousAccess;
+			int j, tick = 0;
+			for(j = 0; j < 32; j++){
+				a->bitString[j] = lruList->top->bitString[j];
+			}
 
-
+			while(a->nextAccess != NULL){
+				printf("%d - %x\n", tick, a->hexVal);
+				a = a->nextAccess;
+				tick++;
+			}
+			printf("%d - %x\n\n", tick, a->hexVal);
+}
+void printVMSQueue(){
+	printf("\n");
+	struct Access* a = (struct Access*)malloc(sizeof(struct Access)); //a is used to iterate through lruList
+	a = vmsQueue->front;
+	int j, tick = 0;
+	for(j = 0; j < 32; j++){
+		a->bitString[j] = vmsQueue->front->bitString[j];
+	}
+		while(a->nextAccess != NULL){
+		printf("%d - %x (%d)\n", tick, a->hexVal, a->referenced);
+		a = a->nextAccess;
+		tick++;
+	}
+	printf("%d - %x (%d)\n", tick, a->hexVal, a->referenced);
+}
